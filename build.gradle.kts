7138d09866e94246a160e269fbce747369b1afab
t0xodile <thomas.stacey@gmail.com>
2025-09-18 13:32:36 +0200

    Added option to disable tagging.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "2.1.21"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
 group = "com.t0xodile"
@@ -13,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("net.portswigger.burp.extensions:montoya-api:2025.6")
+    implementation("net.portswigger.burp.extensions:montoya-api:2025.7")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.json:json:20250517")
